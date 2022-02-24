4cf792e952d4bf5563b77db67c7a6ef501a07397
zanecosmo <zanecosmo@gmail.com>
2022-02-24 12:00:50 -0500

    finished everything except for contact form, and html/css refactor, and mobile responsiveness

---
+++ site.js
@@ -0,0 +1,7 @@
+const contactButton = document.querySelector(".contact-button");
+const portfolioButton = document.querySelector(".portfolio-button");
+const logoButton = document.querySelector(".logo");
+
+contactButton.addEventListener("click", () => window.scroll(0, 2669));
+portfolioButton.addEventListener("click", () => window.scroll(0, 1404));
+logoButton.addEventListener("click", () => window.scroll(0, 0));
